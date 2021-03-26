e66d757c1edeeab828f5cb9b54557515f051db58
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-26 12:04:58 -0400

    💄 add sustom painter que es una ola

--- lib/src/widget/header.dart
+++ lib/src/widget/header.dart
@@ -198,3 +198,45 @@ class _Header4Painter extends CustomPainter {
     return true;
   }
 }
+
+class Header5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      /* color: Color(0xff33FFDE), */
+      child: CustomPaint(
+        painter: _Header5Painter(),
+      ),
+    );
+  }
+}
+
+class _Header5Painter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    /* propiedades */
+
+    paint.color = Color(0xff33FFDE);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+    final path = new Path();
+    path.lineTo(0, size.height * 0.30);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.35,
+        size.width * 0.50, size.height * 0.30);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.25, size.width, size.height * 0.30);
+
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
