89c6e452d21dc6bdf4a357ca92cb57bdcafd8e0e
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-26 10:54:55 -0400

    💄 diagonal realizada con custom pointer

--- lib/src/widget/header.dart
+++ lib/src/widget/header.dart
@@ -24,3 +24,46 @@ class HeaderBorder extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      /* color: Color(0xff33FFDE), */
+      child: CustomPaint(
+        painter: _HeaderDiagolanlPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagolanlPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    /* propiedades */
+
+    paint.color = Color(0xff33FFDE);
+    /* paint.style = PaintingStyle.stroke; */ /*solo para bolder */
+    paint.style = PaintingStyle.fill; /*solo para bolder */
+    paint.strokeWidth = 2;
+    final path = new Path();
+
+    /* dibujar con el path y el paint */
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    /* path.lineTo(0, size.height * 0.5); */
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
