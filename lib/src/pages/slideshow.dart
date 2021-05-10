88f67171f11c5268da1feffa24951a33eed08123
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-05-09 21:30:51 -0400

    agregamos diseno de nuestro slider

--- lib/src/pages/slideshow.dart
+++ lib/src/pages/slideshow.dart
@@ -8,8 +8,79 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Center(child: Text('hola mundo')),
+        child: Column(
+          children: <Widget>[
+            Expanded(child: _Slider()),
+            _Dots(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(),
+          _Dot(),
+          _Dot(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 12,
+      height: 12,
+      margin: EdgeInsets.symmetric(
+        horizontal: 5,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
+
+// SvgPicture.asset('assets/svg/slide-1.svg')
+class _Slider extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: <Widget>[
+          _Slide('assets/svg/slide-1.svg'),
+          _Slide('assets/svg/slide-2.svg'),
+          _Slide('assets/svg/slide-3.svg'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String svg;
+  _Slide(this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
     );
   }
 }
