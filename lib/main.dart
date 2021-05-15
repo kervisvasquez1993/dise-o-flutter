24aba4ca3aafc141d23afb3ff8949d3a48e14355
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-05-15 19:07:31 -0400

    05-12 cambiamos sliderShow para aceptar parametro

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:appredvital/src/pages/animaciones_pages.dart';
 // import 'package:appredvital/src/labs/circular_progress_page.dart';
 // import 'package:appredvital/src/pages/graficas_circulares_page.dart';
 // import 'package:appredvital/src/pages/header_page.dart';
-import 'package:appredvital/src/pages/slideshow.dart';
+import 'package:appredvital/src/labs/slideshow.dart';
+import 'package:appredvital/src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Diseño app',
-      home: SlideShowPage(),
+      home: SlidershowPage(),
     );
   }
 }
---
+++ lib/src/pages/slideshow_page.dart
@@ -0,0 +1,9 @@
+import 'package:appredvital/src/widget/slideshow.dart';
+import 'package:flutter/material.dart';
+
+class SlidershowPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: slideshow());
+  }
+}
