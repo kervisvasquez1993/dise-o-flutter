dd5a647fba8f7dd8bd724771ac3c32e0edd0253a
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-05-23 19:15:16 -0400

    agregamos stylos a los items en nuestro layouts

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -13,23 +13,37 @@ class PinterestPage extends StatelessWidget {
 }
 
 class _PinteresGrip extends StatelessWidget {
+  final List<int> items = List.generate(200, (index) => index);
   @override
   Widget build(BuildContext context) {
     return new StaggeredGridView.countBuilder(
       crossAxisCount: 4,
-      itemCount: 8,
-      itemBuilder: (BuildContext context, int index) => new Container(
-          color: Colors.green,
-          child: new Center(
-            child: new CircleAvatar(
-              backgroundColor: Colors.white,
-              child: new Text('$index'),
-            ),
-          )),
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
       staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
+          new StaggeredTile.count(2, index.isEven ? 2 : 3),
       mainAxisSpacing: 4.0,
       crossAxisSpacing: 4.0,
     );
+  }
+}
+
+class _PinterestItem extends StatelessWidget {
+  final int index;
+  _PinterestItem(this.index);
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+        ),
+        child: new Center(
+          child: new CircleAvatar(
+            backgroundColor: Colors.white,
+            child: new Text('$index'),
+          ),
+        ));
   }
 }
